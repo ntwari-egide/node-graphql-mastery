0f056fc2833432d6eb76eab37b211d12eabcb40d
ntwari <ntwariegide2@gmail.com>
2021-06-20 00:04:44 +0200

    Fix bug in initialzing gql

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const expressGraphQl = require('express-graphql').graphqlHTTP
 const app = express()
 
+app.use('/graphql',expressGraphQl({}))
 app.listen(5000,()=>{
     console.log("Server running ....");
 })
