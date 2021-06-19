db14001bdb6ed5a82f83703139850f792c4c3099
ntwari <ntwariegide2@gmail.com>
2021-06-20 01:11:01 +0200

    resolving error

--- server.js
+++ server.js
@@ -111,17 +111,20 @@ const RootMutationQuery = new GraphQLObjectType({
     name: 'Root mutations',
     description: '',
     fields: () => ({
-        addBook = {type: BookType},
-        description: 'add book',
-        args: {
-            name: {type: GraphQLNonNull(GraphQLString)},
-            authorId: {type: GraphQLNonNull(GraphQLInt)}
-        },
-        resolve: () => {
-            const book = {id: books.length + 1, name: args.name, authorId: args.authorId}
-            books.push(book)
-
-            return book
+        addBook = {
+            type: BookType,
+            description: 'add book',
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)},
+                authorId: {type: GraphQLNonNull(GraphQLInt)}
+            },
+            resolve: () => {
+                const book = {id: books.length + 1, name: args.name, authorId: args.authorId}
+                books.push(book)
+    
+                return book
+            }
+        
         }
     })
 })
